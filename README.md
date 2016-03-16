5781af623e0a2f9532195610ad162e7bffb6e012
Ohisa Yamada <yamada-o@klab.com>
2016-03-16 16:59:42 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# tmux2slack
---
+++ tmux2slack-snippet.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eu
+
+token=$(cat ~/etc/slack-api-token.txt)
+filename='tmux-buffer.txt'
+
+channels="$1"
+
+title="$LOGNAME@$(uname -n)"
+content=$(tmux show-buffer 2> /dev/null)
+
+curl --data-urlencode token="${token}" \
+     --data-urlencode filename="${filename}" \
+     --data-urlencode channels="${channels}" \
+     --data-urlencode title="${title}" \
+     --data-urlencode content="${content}" \
+     https://slack.com/api/files.upload \
+     > /dev/null 2>&1
